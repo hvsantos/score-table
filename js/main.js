52c5992d68e5c65e1d73ede05cb93df9a0fb96be
Henrique Santos <hv_santos@outlook.com>
2022-03-15 14:23:02 -0300

    create function to print all elements on the screen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-// function genTable() {
-//   document.querySelector('#refresh-button').setAttribute('hidden', '')
-// }
+function generateTable() {
+  document.querySelector('#player-rows').innerHTML = ''
+  for (i = 0; i < score_list.length; i++) {
+    let points =
+      score_list[i].win * win_value +
+      score_list[i].draw * draw_value +
+      score_list[i].defeat * defeat_value
+
+    document.querySelector('#player-rows').innerHTML += `<tr>
+      <td>${score_list[i].player}</td>
+      <td>${score_list[i].win}</td>
+      <td>${score_list[i].draw}</td>
+      <td>${score_list[i].defeat}</td>
+      <td>${points}</td>
+      <td><button class="main-button -prow" onClick="addWin(${i})">Vitória</button></td>
+      <td><button class="main-button -prow" onClick="addDraw(${i})">Empate</button></td>
+      <td><button class="main-button -prow" onClick="addDefeat(${i})">Derrota</button></td>
+      </tr>`
+    points = 0
+  }
+  checkWinDefeat()
+  // checkDraw()
+}
+
+function addWin(index) {
+  score_list[index].win += 1
+  generateTable()
+}
+function addDraw(index) {
+  score_list[index].draw += 1
+  generateTable()
+}
+function addDefeat(index) {
+  score_list[index].defeat += 1
+  generateTable()
+}
