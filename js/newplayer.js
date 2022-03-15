8387f27438da08592467f087c2f3005065750642
Henrique Santos <hv_santos@outlook.com>
2022-03-15 14:22:05 -0300

    add a function to add new players

---
+++ js/newplayer.js
@@ -0,0 +1,28 @@
+const button_newp = document.querySelector('#new-player')
+button_newp.addEventListener('click', newPlayer)
+function newPlayer(event) {
+  event.preventDefault()
+  let arrayMenu = document.querySelectorAll('.addplayer')
+  for (let i = 0; i < arrayMenu.length; i++) {
+    arrayMenu[i].removeAttribute('hidden')
+  }
+  button_newp.setAttribute('hidden', '')
+}
+
+const input_new = document.querySelector('#input-new')
+const button_pushnew = document.querySelector('#button-new')
+button_pushnew.addEventListener('click', submitNewPlayer)
+function submitNewPlayer(event) {
+  event.preventDefault()
+  inputname = input_new.value
+  score_list.push({
+    player: `${inputname}`,
+    win: 0,
+    draw: 0,
+    defeat: 0
+  })
+  button_pushnew.setAttribute('hidden', '')
+  input_new.setAttribute('hidden', '')
+  button_newp.removeAttribute('hidden')
+  generateTable()
+}
