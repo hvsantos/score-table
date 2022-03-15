8f449976093b8d03defdb395e51608bcc69b7653
Henrique Santos <hv_santos@outlook.com>
2022-03-15 10:00:26 -0300

    declare constants and add a function to refresh the values multipliers

--- js/values.js
+++ js/values.js
@@ -1,1 +1,36 @@
 const score_list = []
+const winInput = document.querySelector('#winMult')
+const drawInput = document.querySelector('#drawMult')
+const defeatInput = document.querySelector('#defeatMult')
+
+let win_value = 0
+let draw_value = 0
+let defeat_value = 0
+
+winInput.addEventListener('input', refreshValueWin)
+drawInput.addEventListener('input', refreshValueDraw)
+defeatInput.addEventListener('input', refreshValueDefeat)
+
+function refreshValueWin() {
+  win_value = document.querySelector('#winMult').value
+  if (win_value == '') {
+    win_value = 0
+  }
+  document.querySelector('#refresh-button').removeAttribute('hidden')
+}
+
+function refreshValueDraw() {
+  draw_value = drawInput.value
+  if (draw_value == '') {
+    draw_value = 0
+  }
+  document.querySelector('#refresh-button').removeAttribute('hidden')
+}
+
+function refreshValueDefeat() {
+  defeat_value = defeatInput.value
+  if (defeat_value == '') {
+    defeat_value = 0
+  }
+  document.querySelector('#refresh-button').removeAttribute('hidden')
+}
